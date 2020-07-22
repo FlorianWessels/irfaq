68d6c5b508a80757be1dfae0a09c6483b273c9ca
Florian Wessels <flo.wessels@gmail.com>
2020-07-22 17:07:35 +0200

    [TASK] Mainstream ext_emconf file

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,28 +4,24 @@ $EM_CONF['irfaq'] = [
     'title' => 'GEN Modern FAQ',
     'description' => 'FAQ frontend plugin with dynamic or static view which will merge and improve functionality of EXT:faq and EXT:faq_plus into a modern look',
     'category' => 'plugin',
-    'shy' => 0,
     'version' => '3.0.0-dev',
-    'priority' => '',
-    'loadOrder' => '',
-    'module' => '',
-    'state' => 'beta',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'modify_tables' => '',
-    'clearcacheonload' => 0,
-    'lockType' => '',
-    'author' => 'Leonie Philine Bitto [Netcreators], Tania Morales [Netcreators]',
-    'author_email' => 'extensions@netcreators.com',
-    'author_company' => 'Netcreators',
-    'CGLcompliance' => '',
-    'CGLcompliance_note' => '',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
-            'php' => '7.1.0-7.2.99'
+            'php' => '7.2.0-7.4.99'
         ],
         'conflicts' => [],
         'suggests' => []
-    ]
+    ],
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'clearCacheOnLoad' => false,
+    'author' => 'Leonie Philine Bitto [Netcreators], Tania Morales [Netcreators], Florian Wessels [Leuchtfeuer]',
+    'author_email' => 'extensions@netcreators.com, dev@Leuchtfeuer.com',
+    'author_company' => 'Netcreators, Leuchtfeuer Digital Marketing',
+    'autoload' => [
+        'psr-4' => [
+            'Netcreators\\Irfaq\\' => 'Classes',
+        ],
+    ],
 ];
